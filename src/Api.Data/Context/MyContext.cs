695902715d3542debd2c1ca4336e6685a0e000f1
lucas <lucassouza690@gmail.com>
2021-10-31 12:12:35 -0300

    55. Api.Data - Criar MyContext e Referenciar Api.Data com Api.Domain

---
+++ src/Api.Data/Context/MyContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+  public class MyContext : DbContext
+  {
+    public DbSet<UserEntity> Users { get; set; }
+
+    public MyContext(DbContextOptions<MyContext> contextOptions) : base(contextOptions)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder model)
+    {
+      base.OnModelCreating(model);
+
+    }
+  }
+}
