7c192ab2c3f9189969378702d0981f8af66f7ef9
lucas <lucassouza690@gmail.com>
2021-11-09 20:48:22 -0300

    56. Api.Data - Criar ContextFactory

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+  public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+  {
+    public MyContext CreateDbContext(string[] args)
+    {
+      var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog=webapi_ddd; Integrated Security=True;";
+      var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+      optionsBuilder.UseMySql(connectionString);
+      return new MyContext(optionsBuilder.Options);
+    }
+  }
+}
