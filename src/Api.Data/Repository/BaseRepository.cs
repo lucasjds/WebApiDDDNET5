01f0ff9787fc24c7bfe3297c1f78901c9d5c3683
lucas <lucassouza690@gmail.com>
2021-11-21 16:14:01 -0300

    60. Api.Data - Repositório Genérico Método InsertAsync

---
+++ src/Api.Data/Repository/BaseRepository.cs
@@ -0,0 +1,62 @@
+﻿using Data.Context;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+  public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+  {
+    protected readonly MyContext _context;
+    private DbSet<T> _dataset;
+    public BaseRepository(MyContext context)
+    {
+      _context = context;
+      _dataset = context.Set<T>();
+    }
+
+    public Task<bool> DeleteAsync(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task<T> InsertAsync(T item)
+    {
+      try
+      {
+        if(item.Id == Guid.Empty)
+        {
+          item.Id = Guid.NewGuid();
+        }
+        item.CreateAt = DateTime.UtcNow;
+        _dataset.Add(item);
+        await _context.SaveChangesAsync();
+      }
+      catch
+      {
+        throw;
+      }
+      return item;
+    }
+
+    public Task<T> SelectAsync(Guid id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<T>> SelectAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(T item)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
