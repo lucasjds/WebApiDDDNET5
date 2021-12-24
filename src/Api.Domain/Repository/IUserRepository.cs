69ddf1d51cec925fea5c096581f0b163d266c4fc
Lucas <BairesDev@DESKTOP-3EETS12>
2021-12-24 09:05:00 -0300

    78. Api.Domain - IUserRepository

--- src/Api.Domain/Repository/IUserRepository.cs
+++ src/Api.Domain/Repository/IUserRepository.cs
@@ -1,9 +1,5 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Repository
