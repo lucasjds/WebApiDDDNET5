5af327d5fc55ef872705bae4bf2e8143342aff6c
Lucas <BairesDev@DESKTOP-3EETS12>
2022-01-03 21:25:14 -0300

    83. Api.CrossCutting - Injeção de Dependências

--- src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Data.Context;
+using Data.Implementation;
 using Data.Repository;
 using Domain.Interfaces;
+using Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,6 +14,7 @@ namespace CrossCutting.DependencyInjection
     {
       var conn = "Data Source=127.0.0.1; User ID=root; Password=admin123; Initial Catalog=webapi_ddd; ";
       serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+      serviceCollection.AddScoped<IUserRepository, UserImplementation>();
       serviceCollection.AddDbContext<MyContext>(
                 opt => opt.UseMySql(conn, ServerVersion.AutoDetect(conn))
             );
