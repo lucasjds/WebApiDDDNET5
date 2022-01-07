e113a49fc8f5e893676c4bf8c6888cdb264df16f
Lucas <BairesDev@DESKTOP-3EETS12>
2022-01-06 21:24:03 -0300

    84. Api.Domain - Criar Login Dto (Data Transfer Object)

---
+++ src/Api.Domain/Dtos/LoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos
+{
+  public class LoginDto
+  {
+    [Required(ErrorMessage = "Email é campo obrigatorio")]
+    [EmailAddress(ErrorMessage = "Email em formato invalido")]
+    [StringLength(100, ErrorMessage = "Email deve ter no maximo {1} caracteres")]
+    public string Email { get; set; }
+  }
+}
