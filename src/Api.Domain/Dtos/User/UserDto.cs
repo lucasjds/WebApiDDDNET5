f855b2d9f6001fb29a0c6709711f3cb8d91f7e9a
Lucas <BairesDev@DESKTOP-3EETS12>
2022-02-09 20:51:42 -0300

    93. Criar DTOs para User

---
+++ src/Api.Domain/Dtos/User/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User
+{
+  public class UserDto
+  {
+    [Required(ErrorMessage = "Nome é campo obrigatorio")]
+    [StringLength(60, ErrorMessage = "Nome deve ter no maximo {1} caracteres")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "Email é campo obrigatorio")]
+    [EmailAddress(ErrorMessage = "Email em formato invalido")]
+    [StringLength(100, ErrorMessage = "Email deve ter no maximo {1} caracteres")]
+    public string Email { get; set; }
+  }
+}
