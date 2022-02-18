e9dc0f53e675e0ceb4f25c92274d1e8ecb023fc8
Lucas <BairesDev@DESKTOP-3EETS12>
2022-02-18 14:57:02 -0300

    97. Refatorar IUserService e UserService

--- src/Api.Domain/Interfaces/Services/Users/IUserService.cs
+++ src/Api.Domain/Interfaces/Services/Users/IUserService.cs
@@ -1,18 +1,16 @@
-﻿using Domain.Entities;
+﻿using Api.Domain.Dtos.User;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Interfaces.Services.Users
 {
   public interface IUserService
   {
-    Task<UserEntity> Get(Guid id);
-    Task<IEnumerable<UserEntity>> GetAll();
-    Task<UserEntity> Post(UserEntity userEntity);
-    Task<UserEntity> Put(UserEntity user);
+    Task<UserDto> Get(Guid id);
+    Task<IEnumerable<UserDto>> GetAll();
+    Task<UserDtoCreateResult> Post(UserDto user);
+    Task<UserDtoUpdateResult> Put(UserDto user);
     Task<bool> Delete(Guid id);
   }
 }
--- src/Api.Service/Services/UserService.cs
+++ src/Api.Service/Services/UserService.cs
@@ -1,10 +1,11 @@
-﻿using Domain.Entities;
+﻿using Api.Domain.Dtos.User;
+using AutoMapper;
+using Domain.Entities;
 using Domain.Interfaces;
 using Domain.Interfaces.Services.Users;
+using Domain.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Service.Services
@@ -12,9 +13,12 @@ namespace Service.Services
   public class UserService : IUserService
   {
     private readonly IRepository<UserEntity> _repository;
-    public UserService(IRepository<UserEntity> repository)
+
+    private readonly IMapper _mapper;
+    public UserService(IRepository<UserEntity> repository, IMapper mapper)
     {
       _repository = repository;
+      _mapper = mapper;
     }
 
     public async Task<bool> Delete(Guid id)
@@ -22,24 +26,32 @@ namespace Service.Services
       return await _repository.DeleteAsync(id);
     }
 
-    public async Task<UserEntity> Get(Guid id)
+    public async Task<UserDto> Get(Guid id)
     {
-      return await _repository.SelectAsync(id);
+      var entity = await _repository.SelectAsync(id);
+      return _mapper.Map<UserDto>(entity);
     }
 
-    public async Task<IEnumerable<UserEntity>> GetAll()
+    public async Task<IEnumerable<UserDto>> GetAll()
     {
-      return await _repository.SelectAsync();
+      var list = await _repository.SelectAsync();
+      return _mapper.Map<IEnumerable<UserDto>>(list);
     }
 
-    public async Task<UserEntity> Post(UserEntity userEntity)
+    public async Task<UserDtoCreateResult> Post(UserDto user)
     {
-      return await _repository.InsertAsync(userEntity);
+      var model = _mapper.Map<UserModel>(user);
+      var entity = _mapper.Map<UserEntity>(model);
+      var result = await _repository.InsertAsync(entity);
+      return _mapper.Map<UserDtoCreateResult>(result);
     }
 
-    public async Task<UserEntity> Put(UserEntity user)
+    public async Task<UserDtoUpdateResult> Put(UserDto user)
     {
-      return await _repository.UpdateAsync(user);
+      var model = _mapper.Map<UserModel>(user);
+      var entity = _mapper.Map<UserEntity>(model);
+      var result = await _repository.UpdateAsync(entity);
+      return _mapper.Map<UserDtoUpdateResult>(result);
     }
   }
 }
